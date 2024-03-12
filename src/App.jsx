96e056eb52dbed4f142c9ede230e1eb1386241e3
unknown <you@example.com>
2024-03-13 05:32:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+      <h1 className='text-6xl bg-red-300'>Knowllaged Cafa</h1>
+      
+    </>
+  )
+}
+
+export default App
